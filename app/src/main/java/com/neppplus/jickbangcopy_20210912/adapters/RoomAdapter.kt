1840e59a7276179861127c20c80e60782d46aaa6
조경진 <cho881020@gmail.com>
2021-09-12 14:02:23 +0900

    가격을 상황에 따라 다르게 가공하는 기능 (RoomData에) 추가
      - RoomAdapter에서, 방마다 가진 해당 기능을 활용.

--- app/src/main/java/com/neppplus/jickbangcopy_20210912/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/adapters/RoomAdapter.kt
@@ -30,6 +30,7 @@ class RoomAdapter(
         val addressAndFloorTxt = row.findViewById<TextView>(R.id.addressAndFloorTxt)
         val descriptionTxt = row.findViewById<TextView>(R.id.descriptionTxt)
 
+        priceTxt.text = data.getFormattedPrice()
 
         descriptionTxt.text  =  data.description
 
