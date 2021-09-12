2062f1b8eb5605c855b46b5ac73b1adca7aa74a6
조경진 <cho881020@gmail.com>
2021-09-12 16:08:19 +0900

    주석 삭제

--- app/src/main/java/com/neppplus/jickbangcopy_20210912/datas/RoomData.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/datas/RoomData.kt
@@ -13,10 +13,6 @@ class RoomData(
 
     fun getFormattedFloor() : String {
 
-//        1. 양수인 경우 (1, 3, 5..) =>  "floor층"
-//        2. 0인 경우 => "반지하"
-//        3. 음수인 경우 (-1, -2..) =>  "지하 ?층"
-
         if (this.floor > 0) {
             return "${this.floor}층"
         }
@@ -32,12 +28,7 @@ class RoomData(
 
     fun getFormattedPrice() : String {
 
-//        1. 1억이 안넘는 경우 ( 가격 < 10000)
-//        2. 그 외 : 1억 이상 => ?억 ???
-
         if (this.price < 10000) {
-
-//            8000 => "8,000" 으로 변경.
 
             val formattedStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
 
@@ -45,17 +36,9 @@ class RoomData(
         }
         else {
 
-//            75000 => "7억 5,000"
-
-//            억단위?75000 -> 7,   165000 -> 16  =>  가격 / 10000 했을때, 몫?
-//            정수 / 정수 => 결과도 무조건 정수. (소수점 버림)  168 / 10 => 16
-
             val uk = this.price / 10000
 
-//            천만원까지? 75000 => 5000,  168300 => 8300  =>  가격 % 10000 했을때, 나머지?
-
             val rest = this.price % 10000
-//            천만원단위 -> 컴마 찍기.
             val restCommaStr = NumberFormat.getNumberInstance(Locale.KOREA).format(rest)
 
             val finalStr = "${uk}억 ${restCommaStr}"
