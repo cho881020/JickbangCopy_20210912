d84af07df1d02e2f68208742209a5243097dddd3
조경진 <cho881020@gmail.com>
2021-09-12 12:39:24 +0900

    RoomAdapter 클래스 작업 마무리

--- app/src/main/java/com/neppplus/jickbangcopy_20210912/adapters/RoomAdapter.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.neppplus.jickbangcopy_20210912.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.neppplus.jickbangcopy_20210912.R
 import com.neppplus.jickbangcopy_20210912.datas.RoomData
 
 class RoomAdapter(
@@ -9,6 +13,18 @@ class RoomAdapter(
     val resId: Int,
     val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mList) {
 
+    val mInflater = LayoutInflater.from(mContext)
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return  row
+    }
 
 }
