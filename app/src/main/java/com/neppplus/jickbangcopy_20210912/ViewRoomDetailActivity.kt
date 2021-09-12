1e6e49a2ee9266ef5efc1ae8b64d78c260fe52a5
조경진 <cho881020@gmail.com>
2021-09-12 14:57:10 +0900

    받아낸 방 데이터 활용 -> 실제 문구 반영  - 클래스에 만들어둔 함수들을 재활용

--- app/src/main/java/com/neppplus/jickbangcopy_20210912/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.jickbangcopy_20210912
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20210912.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,10 @@ class ViewRoomDetailActivity : AppCompatActivity() {
 
         val room = intent.getSerializableExtra("roomData") as RoomData
 
+        priceTxt.text = room.getFormattedPrice()
+        descriptionTxt.text = room.description
+        addressTxt.text = room.address
+        floorTxt.text = room.getFormattedFloor()
 
 
     }
