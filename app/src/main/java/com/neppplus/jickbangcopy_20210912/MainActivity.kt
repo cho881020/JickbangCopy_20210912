720d01f3c8cab9204aece396bc3c06634ee22bdd
조경진 <cho881020@gmail.com>
2021-09-12 14:53:31 +0900

    Serializable을 활용한 데이터 클래스 통째로 첨부 / 받아서 사용하기

--- app/src/main/java/com/neppplus/jickbangcopy_20210912/MainActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.jickbangcopy_20210912
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.jickbangcopy_20210912.adapters.RoomAdapter
@@ -29,6 +30,18 @@ class MainActivity : AppCompatActivity() {
 
         mRoomAdapter = RoomAdapter(this, R.layout.room_list_item, mRoomList)
         roomListView.adapter =  mRoomAdapter
+
+
+        roomListView.setOnItemClickListener { adapterView, view, position, l ->
+
+            val clickedRoom = mRoomList[position]
+
+            val myIntent = Intent(this, ViewRoomDetailActivity::class.java)
+            myIntent.putExtra("roomData", clickedRoom)
+            startActivity(myIntent)
+
+        }
+
 
     }
 }
--- app/src/main/java/com/neppplus/jickbangcopy_20210912/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/neppplus/jickbangcopy_20210912/ViewRoomDetailActivity.kt
@@ -2,10 +2,16 @@ package com.neppplus.jickbangcopy_20210912
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.jickbangcopy_20210912.datas.RoomData
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val room = intent.getSerializableExtra("roomData") as RoomData
+
+
+
     }
 }
